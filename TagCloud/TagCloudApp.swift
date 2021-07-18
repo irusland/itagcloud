3f464f726d94d29cd7cd0493fd38fa7c31eb703e
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2021-07-18 17:49:10 +0500

    Initial Commit

---
+++ TagCloud/TagCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagCloudApp.swift
+//  TagCloud
+//
+//  Created by Ruslan Sirazhetdinov on 18.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TagCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
